9b403634fba14146b7ac37a82e485df470b8ad90
Yauheni Khvaliuk <hvaluk@gmail.com>
2013-04-14 21:30:01 +0300

    admin: localization for product types

--- warehouse/skill/admin.py
+++ warehouse/skill/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
 
-from skill.models import Product
+from skill.models import ProductType, Product
 
+admin.site.register(ProductType)
 admin.site.register(Product)
 
 # hide unnesesary models from admin part
--- warehouse/skill/models.py
+++ warehouse/skill/models.py
@@ -3,11 +3,21 @@
 from django.db import models
 
 
+class ProductType(models.Model):
+    class Meta:
+        verbose_name = "Позиция"
+        verbose_name_plural = "Позиции"
+    name = models.CharField(max_length=50, verbose_name="Наименование")
+
+    def __unicode__(self):
+        return self.name
+
+
 class Product(models.Model):
     class Meta:
         verbose_name = "Товар"
         verbose_name_plural = "Товары"
-    name = models.CharField(max_length=50, verbose_name="Наименование")
+    route = models.ForeignKey(ProductType, verbose_name="Позиция")
 
     def __unicode__(self):
         return self.name
