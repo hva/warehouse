c5c299cb403f19a3480ccb8f2cedb04d870787b7
Yauheni Khvaliuk <hvaluk@gmail.com>
2013-04-20 23:23:08 +0300

    schema to models

--- warehouse/skill/migrations/0001_initial.py
+++ warehouse/skill/migrations/0001_initial.py
@@ -8,53 +8,144 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'ProductType'
-        db.create_table('skill_producttype', (
+        # Adding model 'Taxonomy'
+        db.create_table('skill_taxonomy', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('parent', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['skill.Taxonomy'], null=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=32)),
+            ('sortorder', self.gf('django.db.models.fields.CharField')(max_length=10)),
         ))
-        db.send_create_signal('skill', ['ProductType'])
+        db.send_create_signal('skill', ['Taxonomy'])
 
         # Adding model 'Product'
         db.create_table('skill_product', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['skill.ProductType'])),
-            ('price', self.gf('django.db.models.fields.DecimalField')(max_digits=10, decimal_places=0)),
-            ('weight', self.gf('django.db.models.fields.FloatField')()),
-            ('item_length', self.gf('django.db.models.fields.FloatField')()),
-            ('full_length', self.gf('django.db.models.fields.FloatField')()),
-            ('coefficient', self.gf('django.db.models.fields.FloatField')()),
-            ('margin', self.gf('django.db.models.fields.IntegerField')()),
-            ('vat', self.gf('django.db.models.fields.IntegerField')()),
+            ('taxonomy', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['skill.Taxonomy'])),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=32)),
         ))
         db.send_create_signal('skill', ['Product'])
 
+        # Adding model 'Shipper'
+        db.create_table('skill_shipper', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=32)),
+        ))
+        db.send_create_signal('skill', ['Shipper'])
+
+        # Adding model 'Buyer'
+        db.create_table('skill_buyer', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=32)),
+        ))
+        db.send_create_signal('skill', ['Buyer'])
+
+        # Adding model 'Income'
+        db.create_table('skill_income', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('product', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['skill.Product'])),
+            ('shipper', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['skill.Shipper'])),
+            ('price', self.gf('django.db.models.fields.DecimalField')(max_digits=10, decimal_places=0)),
+            ('weight', self.gf('django.db.models.fields.FloatField')()),
+            ('margin', self.gf('django.db.models.fields.IntegerField')()),
+            ('vat', self.gf('django.db.models.fields.IntegerField')()),
+            ('create_date', self.gf('django.db.models.fields.DateTimeField')()),
+        ))
+        db.send_create_signal('skill', ['Income'])
+
+        # Adding model 'Outcome'
+        db.create_table('skill_outcome', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('income', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['skill.Income'])),
+            ('buyer', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['skill.Buyer'])),
+            ('weight', self.gf('django.db.models.fields.FloatField')()),
+            ('create_date', self.gf('django.db.models.fields.DateTimeField')()),
+        ))
+        db.send_create_signal('skill', ['Outcome'])
+
+        # Adding model 'Attachment'
+        db.create_table('skill_attachment', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('item_id', self.gf('django.db.models.fields.IntegerField')()),
+            ('item_type', self.gf('django.db.models.fields.IntegerField')()),
+            ('description', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('create_date', self.gf('django.db.models.fields.DateTimeField')()),
+        ))
+        db.send_create_signal('skill', ['Attachment'])
+
 
     def backwards(self, orm):
-        # Deleting model 'ProductType'
-        db.delete_table('skill_producttype')
+        # Deleting model 'Taxonomy'
+        db.delete_table('skill_taxonomy')
 
         # Deleting model 'Product'
         db.delete_table('skill_product')
 
+        # Deleting model 'Shipper'
+        db.delete_table('skill_shipper')
+
+        # Deleting model 'Buyer'
+        db.delete_table('skill_buyer')
+
+        # Deleting model 'Income'
+        db.delete_table('skill_income')
+
+        # Deleting model 'Outcome'
+        db.delete_table('skill_outcome')
+
+        # Deleting model 'Attachment'
+        db.delete_table('skill_attachment')
+
 
     models = {
-        'skill.product': {
-            'Meta': {'object_name': 'Product'},
-            'coefficient': ('django.db.models.fields.FloatField', [], {}),
-            'full_length': ('django.db.models.fields.FloatField', [], {}),
+        'skill.attachment': {
+            'Meta': {'object_name': 'Attachment'},
+            'create_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'item_length': ('django.db.models.fields.FloatField', [], {}),
+            'item_id': ('django.db.models.fields.IntegerField', [], {}),
+            'item_type': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'skill.buyer': {
+            'Meta': {'object_name': 'Buyer'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '32'})
+        },
+        'skill.income': {
+            'Meta': {'object_name': 'Income'},
+            'create_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'margin': ('django.db.models.fields.IntegerField', [], {}),
             'price': ('django.db.models.fields.DecimalField', [], {'max_digits': '10', 'decimal_places': '0'}),
-            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['skill.ProductType']"}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['skill.Product']"}),
+            'shipper': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['skill.Shipper']"}),
             'vat': ('django.db.models.fields.IntegerField', [], {}),
             'weight': ('django.db.models.fields.FloatField', [], {})
         },
-        'skill.producttype': {
-            'Meta': {'object_name': 'ProductType'},
+        'skill.outcome': {
+            'Meta': {'object_name': 'Outcome'},
+            'buyer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['skill.Buyer']"}),
+            'create_date': ('django.db.models.fields.DateTimeField', [], {}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+            'income': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['skill.Income']"}),
+            'weight': ('django.db.models.fields.FloatField', [], {})
+        },
+        'skill.product': {
+            'Meta': {'object_name': 'Product'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'taxonomy': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['skill.Taxonomy']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '32'})
+        },
+        'skill.shipper': {
+            'Meta': {'object_name': 'Shipper'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '32'})
+        },
+        'skill.taxonomy': {
+            'Meta': {'object_name': 'Taxonomy'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['skill.Taxonomy']", 'null': 'True'}),
+            'sortorder': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '32'})
         }
     }
 
--- warehouse/skill/models.py
+++ warehouse/skill/models.py
@@ -1,45 +1,50 @@
 # coding=utf-8
 
 from django.db import models
-from decimal import Decimal
 
 
-class ProductType(models.Model):
-    class Meta:
-        verbose_name = 'Позиция'
-        verbose_name_plural = 'Позиции'
-    name = models.CharField(max_length=50, verbose_name='Наименование')
-
-    def __unicode__(self):
-        return self.name
+class Taxonomy(models.Model):
+    parent = models.ForeignKey('self', null=True)
+    title = models.CharField(max_length=32)
+    sortorder = models.CharField(max_length=10)
 
 
 class Product(models.Model):
-    class Meta:
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
+    taxonomy = models.ForeignKey(Taxonomy)
+    title = models.CharField(max_length=32)
 
-    type = models.ForeignKey(ProductType, verbose_name='Наименование')
-    price = models.DecimalField(max_digits=10, decimal_places=0, verbose_name='Приходная цена за 1 тонну без НДС')
-    weight = models.FloatField(verbose_name='Тоннаж (т)')
-    item_length = models.FloatField(verbose_name='Длина штанги (м)')
-    full_length = models.FloatField(verbose_name='Метраж (м)')
-    coefficient = models.FloatField(verbose_name='Коэффициент')
-    margin = models.IntegerField(verbose_name='Наценка (%)')
-    vat = models.IntegerField(verbose_name='Ставка НДС (%)')
 
-    _margin_price = None
-    _margin_vat_price = None
-    _margin_vat_price_meter = None
+class Shipper(models.Model):
+    title = models.CharField(max_length=32)
 
-    def margin_price(self):
-        return self.price * (100 + self.margin) / 100
 
-    def margin_vat_price(self):
-        return self.margin_price() * (100 + self.vat) / 100
+class Buyer(models.Model):
+    title = models.CharField(max_length=32)
 
-    def margin_vat_price_meter(self):
-        return self.margin_vat_price() * Decimal(self.coefficient) / 1000
 
-    # def __unicode__(self):
-    #     return self.name
+class Income(models.Model):
+    product = models.ForeignKey(Product)
+    shipper = models.ForeignKey(Shipper)
+    price = models.DecimalField(max_digits=10, decimal_places=0)
+    weight = models.FloatField()
+    margin = models.IntegerField()
+    vat = models.IntegerField()
+    create_date = models.DateTimeField()
+
+
+class Outcome(models.Model):
+    income = models.ForeignKey(Income)
+    buyer = models.ForeignKey(Buyer)
+    weight = models.FloatField()
+    create_date = models.DateTimeField()
+
+
+class Attachment(models.Model):
+    ATTACHMENT_TYPE = (
+        (0, 'income'),
+        (1, 'shipper'),
+    )
+    item_id = models.IntegerField()
+    item_type = models.IntegerField(choices=ATTACHMENT_TYPE)
+    description = models.CharField(max_length=256)
+    create_date = models.DateTimeField()
