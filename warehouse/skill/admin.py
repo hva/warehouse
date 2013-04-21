629394a89de9979d7aa56f8245385d68d42c8219
Yauheni Khvaliuk <hvaluk@gmail.com>
2013-04-21 13:12:22 +0300

    home: view fixed after models changed

--- warehouse/skill/admin.py
+++ warehouse/skill/admin.py
@@ -1,16 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
+# from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
 
-from skill.models import ProductType, Product
-
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('type', 'price', 'weight', 'item_length', 'full_length', 'coefficient', 'margin', 'vat')
-
-admin.site.register(ProductType)
-admin.site.register(Product, ProductAdmin)
-
 # hide unnesesary models from admin part
-admin.site.unregister(Group)
+# admin.site.unregister(Group)
 admin.site.unregister(Site)
--- warehouse/skill/views.py
+++ warehouse/skill/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from models import Product
 
 
 def home(request):
-    products = Product.objects.all()
-    return render_to_response('index.html', {'products': products}, RequestContext(request))
+    return render_to_response('index.html', {}, RequestContext(request))
