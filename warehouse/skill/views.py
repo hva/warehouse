f18fdcf1ad9e38f288d1cd1ab8b877b4cfc47d12
Yauheni Khvaliuk <hvaluk@gmail.com>
2013-05-04 13:29:44 +0300

    home page with large buttons

--- warehouse/skill/views.py
+++ warehouse/skill/views.py
@@ -5,4 +5,9 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def home(request):
-    return render_to_response('taxomomy.html', {}, RequestContext(request))
+    return render_to_response('home.html', {}, RequestContext(request))
+
+
+@login_required
+def taxonomy(request):
+    return render_to_response('taxonomy.html', {}, RequestContext(request))
--- warehouse/urls.py
+++ warehouse/urls.py
@@ -11,8 +11,12 @@ v1_api.register(TaxonomyResource())
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'skill.views.home'),
+
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+
+    url(r'^$', 'skill.views.home'),
+    url(r'^taxonomy/$', 'skill.views.taxonomy'),
+
     url(r'^api/', include(v1_api.urls)),
 )
