ae717c2032f3def17c721a9bb79e85ea84b1551e
Yauheni Khvaliuk <hvaluk@gmail.com>
2013-05-13 17:34:42 +0300

    taxonomy: 'breadcrumbs' directive started.

---
+++ warehouse/skill/static/js/taxonomy.directives.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+taxonomyDirectives.directive('breadcrumbs', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: '/static/partials/breadcrumbs.html'
+    };
+});
--- warehouse/skill/static/js/taxonomy.js
+++ warehouse/skill/static/js/taxonomy.js
@@ -1,7 +1,8 @@
 var taxonomyServices = angular.module('taxonomy.services', ['ngResource']);
 var taxonomyControllers = angular.module('taxonomy.controllers', []);
+var taxonomyDirectives = angular.module('taxonomy.directives', []);
 
-var taxonomy = angular.module('taxonomy', ['taxonomy.services','taxonomy.controllers']);
+var taxonomy = angular.module('taxonomy', ['taxonomy.services','taxonomy.controllers', 'taxonomy.directives']);
 
 taxonomy.config(function($routeProvider) {
     $routeProvider.when('/list', {
