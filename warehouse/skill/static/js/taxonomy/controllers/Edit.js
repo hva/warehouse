9a9e6f04b823199d77bc153c28beb91b7da07b30
Yauheni Khvaliuk <hvaluk@gmail.com>
2013-06-08 12:47:04 +0300

    taxonomy/edit: disable options via directive

--- warehouse/skill/static/js/taxonomy/controllers/Edit.js
+++ warehouse/skill/static/js/taxonomy/controllers/Edit.js
@@ -2,37 +2,23 @@ angular.module('taxonomy.controllers').controller('TaxonomyEditController', func
 
     var original;
 
-    function isDisabledItem(x) {
-        return x.sortorder.indexOf($scope.item.sortorder) === 0;
-    }
-
-    function disableOptions() {
-        $('select option').each(function () {
-            var option = $(this),
-                val = option.val();
-            if (val !== '') {
-                var item = $scope.taxonomy.objects[val];
-                option.attr('disabled', isDisabledItem(item));
-            }
-        });
-    }
-
     $scope.utils = TaxonomyUtils;
 
     $scope.item = Taxonomy.get({id: $routeParams.id}, function (i) {
         original = angular.copy(i);
     });
 
-    $scope.taxonomy = Taxonomy.query(function () {
-        // wait for template render and disable some options
-        $timeout(disableOptions, 0);
-    });
+    $scope.taxonomy = Taxonomy.query();
 
     $scope.breadcrumbs = [
         {title: 'главная', url: '/'},
         {title: 'номенклатура', url: '/taxonomy'},
         {title: 'редактирование группы', url: $location.absUrl()}
     ];
+
+    $scope.isDisabledItem = function (x) {
+        return x.sortorder.indexOf($scope.item.sortorder) === 0;
+    }
 
     $scope.submit = function () {
         var item = $scope.item,
---
+++ warehouse/skill/static/js/taxonomy/directives/OptionsDisabled.js
@@ -0,0 +1,35 @@
+// http://jsfiddle.net/alalonde/dZDLg/9/
+
+angular.module('taxonomy.directives').directive('optionsDisabled', function ($parse) {
+    var disableOptions = function (scope, attr, element, data, fnDisableIfTrue) {
+        // refresh the disabled options in the select element.
+        $("option", element).each(function (i, e) {
+            var locals = {},
+                val = $(this).val();
+            if (!isNaN(parseInt(val, 10))) {
+                locals[attr] = data[val];
+                $(this).attr("disabled", fnDisableIfTrue(scope, locals));
+            }
+        });
+    };
+    return {
+        priority: 0,
+        require: 'ngModel',
+        link: function (scope, iElement, iAttrs, ctrl) {
+            // parse expression and build array of disabled options
+            var expElements = iAttrs.optionsDisabled.match(/^\s*(.+)\s+for\s+(.+)\s+in\s+(.+)?\s*/);
+            var attrToWatch = expElements[3];
+            var fnDisableIfTrue = $parse(expElements[1]);
+            scope.$watch(attrToWatch, function (newValue, oldValue) {
+                if (newValue)
+                    disableOptions(scope, expElements[2], iElement, newValue, fnDisableIfTrue);
+            }, true);
+            // handle model updates properly
+            scope.$watch(iAttrs.ngModel, function (newValue, oldValue) {
+                var disOptions = $parse(attrToWatch)(scope);
+                if (newValue && disOptions)
+                    disableOptions(scope, expElements[2], iElement, disOptions, fnDisableIfTrue);
+            });
+        }
+    };
+});
