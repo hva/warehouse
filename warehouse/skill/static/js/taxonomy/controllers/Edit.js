1358cce00af4dae15ce65342700c9ed3be27acc9
Yauheni Khvaliuk <hvaluk@gmail.com>
2013-06-08 12:05:40 +0300

    taxonomy/edit: disable parents using jQuery.

--- warehouse/skill/static/js/taxonomy/controllers/Edit.js
+++ warehouse/skill/static/js/taxonomy/controllers/Edit.js
@@ -1,21 +1,38 @@
-angular.module('taxonomy.controllers').controller('TaxonomyEditController', function ($scope, $location, $routeParams, Taxonomy, TaxonomyUtils, TaxonomySortorder) {
+angular.module('taxonomy.controllers').controller('TaxonomyEditController', function ($scope, $location, $routeParams, $timeout, Taxonomy, TaxonomyUtils, TaxonomySortorder) {
 
     var original;
 
-    Taxonomy.get({id: $routeParams.id}, function (i) {
-        $scope.item = i;
+    function isDisabledItem(x) {
+        return x.sortorder.indexOf($scope.item.sortorder) === 0;
+    }
+
+    function disableOptions() {
+        $('select option').each(function () {
+            var option = $(this),
+                val = option.val();
+            if (val !== '') {
+                var item = $scope.taxonomy.objects[val];
+                option.attr('disabled', isDisabledItem(item));
+            }
+        });
+    }
+
+    $scope.utils = TaxonomyUtils;
+
+    $scope.item = Taxonomy.get({id: $routeParams.id}, function (i) {
         original = angular.copy(i);
     });
 
-    $scope.taxonomy = Taxonomy.query();
+    $scope.taxonomy = Taxonomy.query(function () {
+        // wait for template render and disable some options
+        $timeout(disableOptions, 0);
+    });
 
     $scope.breadcrumbs = [
         {title: 'главная', url: '/'},
         {title: 'номенклатура', url: '/taxonomy'},
         {title: 'редактирование группы', url: $location.absUrl()}
     ];
-
-    $scope.utils = TaxonomyUtils;
 
     $scope.submit = function () {
         var item = $scope.item,
