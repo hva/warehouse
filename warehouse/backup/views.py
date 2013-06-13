d5a3a504866a5bd5a58a990a14285c5e4309f0ab
Yauheni Khvaliuk <hvaluk@gmail.com>
2013-06-13 17:40:08 +0300

    backup: import using 'dumpdata' command

--- warehouse/backup/views.py
+++ warehouse/backup/views.py
@@ -1,6 +1,21 @@
+import time
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponse
+from django.core.management import call_command
 
 
 def home(request):
     return render_to_response('backup.html', {}, RequestContext(request))
+
+
+def import_data(request):
+    filename = 'skill__%s.json' % time.strftime('%Y%m%d_%H%M%S')
+
+    response = HttpResponse(mimetype='application/force-download')
+    response['Content-Disposition'] = 'attachment; filename=%s' % filename
+
+    call_command('dumpdata', 'skill', stdout=response, natural=True, indent=2)
+
+    return response
--- warehouse/urls.py
+++ warehouse/urls.py
@@ -12,12 +12,18 @@ v1_api.register(TaxonomyResource())
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
+    # accounts
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 
+    # skill
     url(r'^$', 'skill.views.home'),
     url(r'^taxonomy/$', 'skill.views.taxonomy'),
-    url(r'^backup/$', 'backup.views.home'),
 
+    # backup
+    url(r'^backup/$', 'backup.views.home'),
+    url(r'^backup/import$', 'backup.views.import_data'),
+
+    # api
     url(r'^api/', include(v1_api.urls)),
 )
