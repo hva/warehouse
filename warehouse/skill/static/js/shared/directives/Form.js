2cd2690208fbd59c7c86b9827bc7e050228c9432
Yauheni Khvliuk <hvaluk@gmail.com>
2013-07-01 12:30:57 +0300

    [shared] form-element label attached

--- warehouse/skill/static/js/shared/directives/Form.js
+++ warehouse/skill/static/js/shared/directives/Form.js
@@ -6,30 +6,30 @@ angular.module('wh.shared.form', [])
 
     .directive('whFormElement', function (labelColumns, inputColumns) {
 
-//        function findInput(el) {
-//            var input = el.find('input');
-//            if (!angular.isElement(input)) {
-//                input = el.find('select');
-//            }
-//            return input;
-//        }
+        function findInput(el) {
+            var input = el.find('input');
+            if (!input.length) {
+                input = el.find('select');
+            }
+            return input;
+        }
 
         return {
             restrict: 'A',
             transclude: true,
             replace: true,
-            templateUrl: '/static/js/shared/views/form/text.html',
+            templateUrl: '/static/js/shared/views/form/element.html',
+            templateUrl: '/static/js/shared/views/form/element.html',
             scope: {
                 label: '@'
             },
             link: function (scope, element) {
                 scope.labelColumns = labelColumns;
                 scope.inputColumns = inputColumns;
-
-//                var input = findInput(element),
-//                    id = input.attr('name') + '-label';
-//                scope.inputId = id;
-//                input.attr('id', id);
+                var input = findInput(element),
+                    id = input.attr('name') + '-label';
+                scope.inputId = id;
+                input.attr('id', id);
             }
         };
     })
