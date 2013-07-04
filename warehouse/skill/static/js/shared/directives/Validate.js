55b6e9d4e124b1185f6118447d8bff2e322aaf57
Yauheni Khvliuk <hvaluk@gmail.com>
2013-07-04 17:25:41 +0300

    [warehouse/add] form validating fixed

--- warehouse/skill/static/js/shared/directives/Validate.js
+++ warehouse/skill/static/js/shared/directives/Validate.js
@@ -1,32 +1,27 @@
 angular.module('wh.shared.validate', [])
 
-//    .constant('INTEGER_REGEXP', /^\-?\d*$/)
+    .constant('INTEGER_REGEXP', /^\-?\d+$/)
     .constant('FLOAT_REGEXP', /^\-?\d+((\.|\,)\d+)?$/)
 
-//    .directive('whFormatPrice', function (INTEGER_REGEXP) {
-//        return {
-//            require: '?ngModel',
-//            link: function (scope, elem, attrs, ctrl) {
-//                if (!ctrl) return;
-//
-//                ctrl.$parsers.unshift(function (viewValue) {
-//                    var isValid = false,
-//                        value = undefined;
-//
-//                    if (INTEGER_REGEXP.test(viewValue)) {
-//                        var intValue = parseInt(viewValue, 10);
-//                        if (intValue > 0) {
-//                            value = intValue;
-//                            isValid = true;
-//                        }
-//                    }
-//                    ctrl.$setValidity('price', isValid);
-//                    return value;
-//                });
-//
-//            }
-//        };
-//    })
+    .directive('whInt', function (INTEGER_REGEXP) {
+        return {
+            require: '?ngModel',
+            link: function (scope, elem, attrs, ctrl) {
+                if (!ctrl) return;
+
+                ctrl.$parsers.unshift(function (viewValue) {
+                    if (INTEGER_REGEXP.test(viewValue)) {
+                        ctrl.$setValidity('int', true);
+                        return parseInt(viewValue, 10);
+                    } else {
+                        ctrl.$setValidity('int', false);
+                        return;
+                    }
+                });
+
+            }
+        };
+    })
 
     .directive('whFloat', function (FLOAT_REGEXP) {
         return {
